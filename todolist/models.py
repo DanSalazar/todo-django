22659b2e78ca742810ce18a2ecf2dd616280680f
Dan <danielsalazar.cc@gmail.com>
2021-11-07 18:05:29 -0400

    Setting app

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class TodoList(models.Model):
+    title = models.CharField(max_length=250)
+    created = models.DateField(default=datetime.date.today)
+    due_date = models.DateField(default=datetime.date.today)
+    
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
