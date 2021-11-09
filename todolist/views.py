ff8e15ef25842240845b3a59c4a37f6c376fd582
Dan <danielsalazar.cc@gmail.com>
2021-11-09 15:38:25 -0400

    feat(app): Add views and request management

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import TodoList
 
-# Create your views here.
+def index(request):
+	todos = TodoList.objects.all()
+	if request.method == 'POST':
+		if 'addTodo' in request.POST:
+			todo_title = request.POST['todo-name']
+			todo = TodoList(title=todo_title)
+			todo.save()
+			return redirect('/')
+	if 'deleteTodo' in request.POST:
+		todo_search = request.POST['deleteTodo']
+		todo_to_delete = TodoList.objects.filter(title=todo_search)  
+		todo_to_delete.delete()
+		return redirect('/')
+
+	return render(request, 'home.html', { 'todos': todos })
+   
